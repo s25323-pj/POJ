ba42f7535fb77d634f7b735724e42ef1a290bf98
s25323-pj <92204632+s25323-pj@users.noreply.github.com>
2022-03-19 16:17:33 +0100

    main

---
+++ kolokwium2/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Main{
+    public static void main(String[] args){
+        String[] tablica = {"1","6","1"};
+        System.out.println(sum(tablica));
+    }
+    public static String sum(String[] array){
+        int globalsum=0;
+        for(int i = 0; i < array.length; i++)
+        {
+            int j=Integer.parseInt(array[i]);
+            globalsum+=j;
+        }
+        return String.valueOf(globalsum);
+    }
+}
