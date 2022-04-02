8af60c6eee5856357c9f04a7aa528bd6b456ff09
s25323-pj <92204632+s25323-pj@users.noreply.github.com>
2022-04-02 17:07:33 +0200

    Add files via upload

---
+++ kolos02.04/TrojkatRownoboczny.java
@@ -0,0 +1,24 @@
+package Trojkaty;
+
+public class TrojkatRownoboczny {
+    public TrojkatRownoboczny(double podstawa, double podstawa1) {
+
+    }
+
+    public TrojkatRownoboczny(double podstawa) {
+
+    }
+
+    public double wysokosc() {
+        return 0;
+    }
+
+    public double wysokosc(int i) {
+        TrojkatRownoboczny trojkat1;
+        double ramie;
+        double podstawa;
+        if (trojkat1.wysokosc((int) 2.5) == Math.sqrt(ramie * ramie - podstawa * podstawa / 4.))
+
+    }
+}
+
