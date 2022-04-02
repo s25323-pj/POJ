92da721a21f4d3f2a027db23f5f444a976da4f29
s25323-pj <92204632+s25323-pj@users.noreply.github.com>
2022-04-02 17:05:29 +0200

    Add files via upload

---
+++ TrojkatRownoramienny.java
@@ -0,0 +1,11 @@
+package Trojkaty;
+
+public class TrojkatRownoramienny {
+    public TrojkatRownoramienny(double podstawa, double ramie) {
+
+    }
+
+    public double wysokosc(double v) {
+        return v;
+    }
+}
